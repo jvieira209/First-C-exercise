06455ae318726934c835a942e72e5d43d4b85674
John Vieira <johnvieira209@gmail.com>
2024-10-24 23:39:19 -0700

    Update MyFirstC#_HelloWorld.cs

--- MyFirstC#_HelloWorld.cs
+++ MyFirstC#_HelloWorld.cs
@@ -2,3 +2,8 @@
 Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code.");
+
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+// This code helps demonstrate the difference between the two methods. A new line is appended by Console.WriteLine(), and Console.Write() prints the output on the current line.
